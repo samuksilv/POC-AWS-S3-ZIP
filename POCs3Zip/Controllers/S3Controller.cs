98e43680d98cfcc9e387af640d296e5901c3ea77
samuel.carvalho <samuel.carvalho@mag-it.com.br>
2019-12-12 09:37:37 -0300

    initial commit

---
+++ POCs3Zip/Controllers/S3Controller.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace POCs3Zip.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class S3Controller : ControllerBase
+    {
+
+        private readonly string _keyId;
+        private readonly string _keySecret;
+        private readonly string _bucketName;
+        private readonly string _baseDownloadLocation;
+        private readonly RegionEndpoint regionEndpoint = RegionEndpoint.USEast2;
+
+        private readonly ILogger<S3Controller> _logger;
+
+        public S3Controller(ILogger<S3Controller> logger)
+        {
+            _keyId = Environment.GetEnvironmentVariable("AWS_KEY_ID");
+            _keySecret = Environment.GetEnvironmentVariable("AWS_KEY_SECRET");
+            _bucketName = Environment.GetEnvironmentVariable("AWS_BUCKET_NAME");
+            _baseDownloadLocation = Path.GetTempPath();
+
+            logger.LogInformation($"AWS_KEY_ID={_keyId}");
+            logger.LogInformation($"AWS_KEY_SECRET={_keySecret}");
+            logger.LogInformation($"AWS_BUCKET_NAME={_bucketName}");
+            _logger = logger;
+        }
+
+        [HttpGet("download/directory")]
+        public async Task<IActionResult> DownloadDirectoryFromS3()
+        {
+            _logger.LogInformation($"AWS_KEY_ID={_keyId}");
+            _logger.LogInformation($"AWS_KEY_SECRET={_keySecret}");
+            _logger.LogInformation($"AWS_BUCKET_NAME={_bucketName}");
+
+            string downloadLocation = _baseDownloadLocation + Guid.NewGuid().ToString();
+            using (var client = new AmazonS3Client(_keyId, _keySecret, regionEndpoint))
+            {
+                return await DownloadDirectoryFromS3Async(client, downloadLocation);
+            }
+        }
+
+        [HttpGet("download/searchByKeys")]
+        public async Task<IActionResult> GetObjectsFromS3([FromQuery]string[] keys)
+        {
+            string downloadLocation = _baseDownloadLocation + Guid.NewGuid().ToString();
+
+            if (!keys.Any())
+                return BadRequest("Infome alguma chave para buscar");
+
+            using (var client = new AmazonS3Client(_keyId, _keySecret, regionEndpoint))
+            {
+                return await SearchByKeysAndDownloadDirectoryFromS3Async(client, keys, downloadLocation);
+            }
+        }
+
+        private async Task<IActionResult> DownloadDirectoryFromS3Async(AmazonS3Client client, string downloadLocation)
+        {
+            string zipFolder = downloadLocation + ".zip";
+            if (!Directory.Exists(downloadLocation))
+                Directory.CreateDirectory(downloadLocation);
+
+            var transferUtility = new TransferUtility(client);
+            await transferUtility.DownloadDirectoryAsync(_bucketName, "\\", downloadLocation);
+            return CreateZip(downloadLocation, zipFolder);
+        }
+
+        private async Task<IActionResult> SearchByKeysAndDownloadDirectoryFromS3Async(AmazonS3Client client, string[] keys, string downloadLocation)
+        {
+            ListObjectsV2Request request = new ListObjectsV2Request
+            {
+                BucketName = _bucketName,
+                MaxKeys = keys.Length,
+            };
+
+            string zipFolder = downloadLocation + ".zip";
+            if (!Directory.Exists(downloadLocation))
+                Directory.CreateDirectory(downloadLocation);
+
+            IEnumerable<Task> tasks = keys.Select(key =>
+            {
+                try
+                {
+                    string filename = downloadLocation + "\\" + key;
+
+                    if (System.IO.File.Exists(filename)) return Task.CompletedTask;
+
+                    GetObjectResponse objResponse = client.GetObjectAsync(_bucketName, key, default).GetAwaiter().GetResult();
+                    return objResponse.WriteResponseStreamToFileAsync(filename, true, default);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+
+            });
+
+            await Task.WhenAll(tasks);
+
+            return CreateZip(downloadLocation, zipFolder);
+        }
+
+        private IActionResult CreateZip(string source, string zipFolder)
+        {
+            ZipFile.CreateFromDirectory(source, zipFolder, CompressionLevel.Fastest, false);
+
+            string contentType = "application/zip";
+            HttpContext.Response.ContentType = contentType;
+
+
+            var result = new FileContentResult(System.IO.File.ReadAllBytes(zipFolder), contentType)
+            {
+                FileDownloadName = $"teste.zip"
+            };
+
+            Directory.Delete(source, true);
+            System.IO.File.Delete(zipFolder);
+
+            return result;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# POC sobre download de arquivos e zip de arquivos de um bucket do aws S3
+
+
+## Objetivo da POC
+
+Nessa poc eu estou baixando arquivos de um bucket do s3, zipando e retornando o zip para download.
+
+## Como rodar o projeto
+
+
+- Para rodar o projeto, crie um arquivo ".env" baseando se no arquivo "example.env" no diretório principal, mesmo nível do arquivo "docker-compose.yml". Nesse arquivo .env são salvas as configurações do S3, bem como informações importantes para acessar-lo, como se trata de uma informação sigilosa eu gerencio essas informações com váriaveis de ambiente.
+    - Váriaveis de ambientes:
+
+|  Várivel       | Descrição            | 
+|---             |---                   |
+|AWS_KEY_ID      |Chave publica do S3   |
+|AWS_KEY_SECRET  |Chave privada do S3   |
+|AWS_BUCKET_NAME |Nome do Bucket no S3  | 
+
+
+ - Após criar o arquico com as váriaveis de ambeite basta ter o docker instalado e rodar o seguinte comando no seu terminal:
+
+> docker-compose up 
+
+## Técnologias usadas
+
+- :aws: AWS S3
+- :csharp: :dotnet: Asp.net Core
+- :whale: Docker /Docker-compose
